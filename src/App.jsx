37a35e1cc2f8c85873b17f334881c7beeb50e555
TiborKopca <tibor.kopca@gmail.com>
2024-10-30 16:51:29 +0100

    button + logic created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logoTK from "./img/TKlogo.svg";
+import "./App.css";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numClicks, setNumClicks] = useState(0);
+
+  //BUTTON HANDLER
+  const handleClick = () => {
+    setNumClicks(numClicks + 1);
+    console.log("number of clicks: ", numClicks);
+  };
+  const handleClickReset = () => {
+    setNumClicks(0);
+    console.log("Reset, number of clicks: ", numClicks);
+  };
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="logo-container">
+        <img 
+          src={logoTK} 
+          className="logo tk" 
+          alt="logo Tibor Kopca" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <main className="main__container">
+        <h1>Contador de clicks</h1>
+        {/* <Contador numClicks= {numClicks} /> */}
+        <Button 
+          text="Start Counter" 
+          isButtonClick={true}
+          handleClick={ handleClick }
+          />
+          <Button 
+            text="Reset" 
+            isButtonClick={false}
+            handleClick={ handleClickReset }
+            />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import "./Button.css"; // Import the CSS file
+
+
+function Button({ text , isButtonClick, handleClick }) {
+  return (
+    <button 
+    className={ isButtonClick ? "button button-click" : "button button-reiniciate" } 
+    onClick={ handleClick}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button
+
+// Define prop types for Button component
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  isButtonClick: PropTypes.bool.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
